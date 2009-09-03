sjnewbury/multilib-overlay

commit 02e013b62e5af9b1b05049decf464b665f4f58df

synched eclasses with portage tree

eclass/distutils.eclass
@@ -1,6 +1,6 @@
 # Copyright 1999-2009 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/eclass/distutils.eclass,v 1.58 2009/08/05 18:34:56 arfrever Exp $
+# $Header: /var/cvsroot/gentoo-x86/eclass/distutils.eclass,v 1.59 2009/08/12 02:24:34 arfrever Exp $
 
 # @ECLASS: distutils.eclass
 # @MAINTAINER:
@@ -12,10 +12,8 @@
 # The distutils eclass is designed to allow easier installation of
 # distutils-based python modules and their incorporation into
 # the Gentoo Linux system.
-#
-# It inherits python, multilib, and eutils
 
-inherit python multilib eutils
+inherit eutils multilib python
 
 case "${EAPI:-0}" in
 	0|1)
@@ -62,6 +60,10 @@ fi
 # @DESCRIPTION:
 # The distutils src_unpack function, this function is exported
 distutils_src_unpack() {
+	if [[ "${EBUILD_PHASE}" != "unpack" ]]; then
+		die "${FUNCNAME}() can be used only in src_unpack() phase"
+	fi
+
 	unpack ${A}
 	cd "${S}"
 
@@ -72,6 +74,10 @@ distutils_src_unpack() {
 # @DESCRIPTION:
 # The distutils src_prepare function, this function is exported
 distutils_src_prepare() {
+	if ! has "${EAPI:-0}" 0 1 && [[ "${EBUILD_PHASE}" != "prepare" ]]; then
+		die "${FUNCNAME}() can be used only in src_prepare() phase"
+	fi
+
 	# remove ez_setup stuff to prevent packages
 	# from installing setuptools on their own
 	rm -rf ez_setup*
@@ -82,6 +88,10 @@ distutils_src_prepare() {
 # @DESCRIPTION:
 # The distutils src_compile function, this function is exported
 distutils_src_compile() {
+	if [[ "${EBUILD_PHASE}" != "compile" ]]; then
+		die "${FUNCNAME}() can be used only in src_compile() phase"
+	fi
+
 	if is_final_abi || (! has_multilib_profile); then
 		if [ -n "${PYTHON_SLOT_VERSION}" ] ; then
 			python=python${PYTHON_SLOT_VERSION}
@@ -100,13 +110,14 @@ distutils_src_compile() {
 	fi
 	einfo Using ${python}
 	if ! has "${EAPI:-0}" 0 1 2 || [[ -n "${SUPPORT_PYTHON_ABIS}" ]]; then
-		build_modules() {
+		building() {
 			echo "$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" "$@"
 			"$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" "$@"
 		}
-		python_execute_function build_modules "$@"
+		python_execute_function building "$@"
 	else
-		${python} setup.py build "$@" || die "compilation failed"
+		echo ${python} setup.py build "$@"
+		${python} setup.py build "$@" || die "Building failed"
 	fi
 }
 
@@ -116,6 +127,10 @@ distutils_src_compile() {
 # It also installs the "standard docs" (CHANGELOG, Change*, KNOWN_BUGS, MAINTAINERS,
 # PKG-INFO, CONTRIBUTORS, TODO, NEWS, MANIFEST*, README*, and AUTHORS)
 distutils_src_install() {
+	if [[ "${EBUILD_PHASE}" != "install" ]]; then
+		die "${FUNCNAME}() can be used only in src_install() phase"
+	fi
+
 	if is_final_abi || (! has_multilib_profile); then
 		if [ -n "${PYTHON_SLOT_VERSION}" ] ; then
 			python=python${PYTHON_SLOT_VERSION}
@@ -138,7 +153,7 @@ distutils_src_install() {
 	python_need_rebuild
 
 	if ! has "${EAPI:-0}" 0 1 2 || [[ -n "${SUPPORT_PYTHON_ABIS}" ]]; then
-		install_modules() {
+		installation() {
 			# need this for python-2.5 + setuptools in cases where
 			# a package uses distutils but does not install anything
 			# in site-packages. (eg. dev-java/java-config-2.x)
@@ -149,7 +164,7 @@ distutils_src_install() {
 			echo "$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" install --root="${D}" --no-compile "$@"
 			"$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" install --root="${D}" --no-compile "$@"
 		}
-		python_execute_function install_modules "$@"
+		python_execute_function installation "$@"
 	else
 		# need this for python-2.5 + setuptools in cases where
 		# a package uses distutils but does not install anything
@@ -158,7 +173,8 @@ distutils_src_install() {
 		pylibdir="$(${python} -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')"
 		[[ -n "${pylibdir}" ]] && dodir "${pylibdir}"
 
-		${python} setup.py install --root="${D}" --no-compile "$@" || die "python setup.py install failed"
+		echo ${python} setup.py install --root="${D}" --no-compile "$@"
+		${python} setup.py install --root="${D}" --no-compile "$@" || die "Installation failed"
 	fi
 
 	DDOCS="CHANGELOG KNOWN_BUGS MAINTAINERS PKG-INFO CONTRIBUTORS TODO NEWS"
@@ -176,6 +192,10 @@ distutils_src_install() {
 # @DESCRIPTION:
 # Generic pyc/pyo cleanup script. This function is exported.
 distutils_pkg_postrm() {
+	if [[ "${EBUILD_PHASE}" != "postrm" ]]; then
+		die "${FUNCNAME}() can be used only in pkg_postrm() phase"
+	fi
+
 	local pylibdir pymod
 	if [[ -z "${PYTHON_MODNAME}" ]]; then
 		for pylibdir in "${ROOT}"/usr/$(get_libdir)/python*; do
@@ -185,7 +205,6 @@ distutils_pkg_postrm() {
 		done
 	fi
 
-	ebegin "Performing cleanup of Python modules..."
 	if [[ -n "${PYTHON_MODNAME}" ]]; then
 		for pymod in ${PYTHON_MODNAME}; do
 			for pylibdir in "${ROOT}"/usr/$(get_libdir)/python*; do
@@ -201,7 +220,6 @@ distutils_pkg_postrm() {
 	else
 		python_mod_cleanup
 	fi
-	eend 0
 }
 
 # @FUNCTION: distutils_pkg_postinst
@@ -209,6 +227,10 @@ distutils_pkg_postrm() {
 # This is a generic optimization, you should override it if your package
 # installs modules in another directory. This function is exported.
 distutils_pkg_postinst() {
+	if [[ "${EBUILD_PHASE}" != "postinst" ]]; then
+		die "${FUNCNAME}() can be used only in pkg_postinst() phase"
+	fi
+
 	local pylibdir pymod
 	if [[ -z "${PYTHON_MODNAME}" ]]; then
 		for pylibdir in "${ROOT}"/usr/$(get_libdir)/python*; do

eclass/cmake-utils.eclass
@@ -1,13 +1,13 @@
 # Copyright 1999-2009 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/eclass/cmake-utils.eclass,v 1.29 2009/06/17 22:39:01 scarabeus Exp $
+# $Header: /var/cvsroot/gentoo-x86/eclass/cmake-utils.eclass,v 1.30 2009/08/08 20:08:02 arfrever Exp $
 
 # @ECLASS: cmake-utils.eclass
 # @MAINTAINER:
 # kde@gentoo.org
 #
 # @CODE
-# Tomáš Chvátal <scarabeus@gentoo.org>
+# TomÃ¡Å¡ ChvÃ¡tal <scarabeus@gentoo.org>
 # Maciej Mrozowski <reavertm@poczta.fm>
 # (undisclosed contributors)
 # Original author: Zephyrus (zephyrus@mirach.it)
@@ -298,6 +298,7 @@ _EOF_
 	mkdir -p "${CMAKE_BUILD_DIR}"
 	pushd "${CMAKE_BUILD_DIR}" > /dev/null
 	debug-print "${LINENO} ${ECLASS} ${FUNCNAME}: mycmakeargs is $cmakeargs"
+	echo cmake ${cmakeargs} "${CMAKE_USE_DIR}"
 	cmake ${cmakeargs} "${CMAKE_USE_DIR}" || die "cmake failed"
 
 	popd > /dev/null

eclass/qt4-build.eclass
@@ -1,6 +1,6 @@
 # Copyright 2007-2009 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/eclass/qt4-build.eclass,v 1.43 2009/07/31 22:18:31 tommy Exp $
+# $Header: /var/cvsroot/gentoo-x86/eclass/qt4-build.eclass,v 1.44 2009/08/11 14:44:16 hwoarang Exp $
 
 # @ECLASS: qt4-build.eclass
 # @MAINTAINER:
@@ -57,8 +57,8 @@ esac
 MY_P=qt-x11-${SRCTYPE}-${MY_PV}
 S=${WORKDIR}/${MY_P}
 
-HOMEPAGE="http://www.qtsoftware.com/"
-SRC_URI="http://get.qtsoftware.com/qt/source/${MY_P}.tar.bz2"
+HOMEPAGE="http://qt.nokia.com/"
+SRC_URI="http://download.qt.nokia.com/qt/source/${MY_P}.tar.bz2"
 
 case "${PV}" in
 	4.4.?) SRC_URI="${SRC_URI} mirror://gentoo/${MY_P}-headers.tar.bz2" ;;

commit 1911ed3bcca66d099206824bca02b2b2d42e807f

Fix distutils.eclass after resync breakage: broken from commit 02e013b62e5af9b1b05049decf464b665f4f58df
We need to use our setarch python command to run the distutils setup.py
otherwise we get the wrong libdir.

eclass/distutils.eclass
@@ -111,8 +111,8 @@ distutils_src_compile() {
 	einfo Using ${python}
 	if ! has "${EAPI:-0}" 0 1 2 || [[ -n "${SUPPORT_PYTHON_ABIS}" ]]; then
 		building() {
-			echo "$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" "$@"
-			"$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" "$@"
+			echo "${python}" setup.py build -b "build-${PYTHON_ABI}" "$@"
+			${python} setup.py build -b "build-${PYTHON_ABI}" "$@"
 		}
 		python_execute_function building "$@"
 	else
@@ -158,11 +158,11 @@ distutils_src_install() {
 			# a package uses distutils but does not install anything
 			# in site-packages. (eg. dev-java/java-config-2.x)
 			# - liquidx (14/08/2006)
-			pylibdir="$("$(PYTHON)" -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')"
+			pylibdir="$(${python} -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')"
 			[[ -n "${pylibdir}" ]] && dodir "${pylibdir}"
 
-			echo "$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" install --root="${D}" --no-compile "$@"
-			"$(PYTHON)" setup.py build -b "build-${PYTHON_ABI}" install --root="${D}" --no-compile "$@"
+			echo "${python}" setup.py build -b "build-${PYTHON_ABI}" install --root="${D}" --no-compile "$@"
+			${python} setup.py build -b "build-${PYTHON_ABI}" install --root="${D}" --no-compile "$@"
 		}
 		python_execute_function installation "$@"
 	else